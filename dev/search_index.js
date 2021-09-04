347c13f6ec2963c5b8060ab8cccd15a982742acf
Documenter.jl <documenter@juliadocs.github.io>
2021-09-04 21:53:59 +0000

    build based on 992743d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Seqeval","category":"page"},{"location":"#Seqeval","page":"Home","title":"Seqeval","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Seqeval.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Seqeval]","category":"page"}]
+}
